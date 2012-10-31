d46e8f7494a934623700fcc3b7e4fb308b76cd34
hzlzh <hzlzh.dev@gmail.com>
2012-11-01 01:24:45 +0800

    add js effect

---
+++ js/main.js
@@ -0,0 +1,51 @@
+jQuery(document).ready(function($) {
+  var LH = function(){
+
+    var update_count = function(length){
+      $('.word-left').html(25-length);
+      $('.word-count').html(length);
+    };
+
+    var get_query = function(){
+      var query = '';
+      $('.letters-pool .letter-box input').each(function(){
+        query += $(this).val();
+      });
+      return query;
+    };
+
+    var init = function(){
+      // jrumble effect init
+      $('.letters-pool .letter-box  input').jrumble().jrumble({
+        speed: 0
+      });
+    };
+
+    return{
+      init:init,
+      get_query:get_query,
+      update_count:update_count
+    };
+  }();
+
+  LH.init();
+
+  $('.letters-pool .letter-box input').mouseenter(function(){
+    $(this).trigger('startRumble');
+    $(this).select();
+  }).mouseleave(function(){
+    var temp = $(this);
+    setTimeout(function() {
+      temp.trigger('stopRumble');
+    }, 420);
+  }).keypress(function(){
+    var temp = $(this);
+    setTimeout(function() {
+      temp.trigger('stopRumble');
+    }, 420);
+    $(this).parent().next().find('input').select().trigger('startRumble');
+  });
+
+  LH.update_count($('.result-box .letter-box span').length);
+
+});
