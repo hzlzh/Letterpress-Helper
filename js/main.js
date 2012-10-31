4490bcc621d6ee99bfaae1651a3dcbab8b7bd86a
hzlzh <hzlzh.dev@gmail.com>
2012-11-01 03:12:29 +0800

    v1.0b released

--- js/main.js
+++ js/main.js
@@ -1,51 +1,77 @@
 jQuery(document).ready(function($) {
-  var LH = function(){
 
-    var update_count = function(length){
-      $('.word-left').html(25-length);
-      $('.word-count').html(length);
-    };
+  var API = 'http://dict.zlz.im/api/index/';
 
-    var get_query = function(){
-      var query = '';
-      $('.letters-pool .letter-box input').each(function(){
-        query += $(this).val();
-      });
-      return query;
-    };
+  var LH = function() {
+      var $obj = $('.letters-pool .letter-box input');
+      $result = $('.result .letter-box');
+      $all = $('.all-box ul');
+      var update_count = function(length) {
+          $('#word-left').html(25 - length);
+          $('#word-count').html(length);
+        };
 
-    var init = function(){
-      // jrumble effect init
-      $('.letters-pool .letter-box input').jrumble().jrumble({
-        speed: 0
-      });
-    };
+      var update_result = function($result, words) {
+          $result.html('<span class="blue">' + words[0].split('').join('</span><span>') + '</span>');
+          for (index in words){
+            $all.append('<li>'+words[index]+'</li>')
+          }
+        };
 
-    return{
-      init:init,
-      get_query:get_query,
-      update_count:update_count
-    };
-  }();
+      var get_query = function() {
+          var query = '';
+          $obj.each(function() {
+            query += $(this).val();
+          });
+          return query;
+        };
+
+      var get_words = function(API, query, length) {
+          var words = '';
+          $.ajaxSettings.async = false;
+          $.getJSON(API + query.toLowerCase() + '/' + length + '?jsoncallback=?', function(data) {
+            if(data != '') {
+              update_result($result, data)
+              update_count(data[0].length)
+            } else update_result($result, 'NULL:(')
+          }, 'json');
+        };
+
+      var init = function() {
+          // jrumble effect init
+          $obj.jrumble().jrumble({
+            speed: 0
+          });
+        };
+
+      return {
+        init: init,
+        get_query: get_query,
+        get_words: get_words
+      };
+    }();
 
   LH.init();
 
-  $('.letters-pool .letter-box input').mouseenter(function(){
+  $('#tool-bar input[type="submit"]').bind('click', function(e) {
+    e.preventDefault();
+    LH.get_words(API, LH.get_query(), $('#word-count').val());
+  });
+
+  $('.letters-pool .letter-box input, .count-board input').mouseenter(function() {
     $(this).trigger('startRumble');
     $(this).select();
-  }).mouseleave(function(){
+  }).mouseleave(function() {
     var temp = $(this);
     setTimeout(function() {
       temp.trigger('stopRumble');
     }, 420);
-  }).keypress(function(){
+  }).keypress(function() {
     var temp = $(this);
     setTimeout(function() {
       temp.trigger('stopRumble');
     }, 420);
     $(this).next().select().trigger('startRumble');
   });
-
-  LH.update_count($('.result-box .letter-box span').length);
 
 });
