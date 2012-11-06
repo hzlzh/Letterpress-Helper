93d8d11f4fd63257b0e8238ce521ad7f1f9ae897
Max Lau <forevervmax@gmail.com>
2012-11-06 09:29:03 +0800

    adjusting code

--- API/python/python.py
+++ API/python/python.py
@@ -7,12 +7,10 @@ from sys import argv
 def read_dic():
 	target = open('dict.txt','r')
 	string = target.read()
-	string_list = string.split(',')
-	return string_list
+	return string.split(',')
 
 def compair(dic_list,input_word,line):
 
-	input_word.sort()
 	result_list = []
 
 	for word in dic_list:
@@ -20,7 +18,7 @@ def compair(dic_list,input_word,line):
 			break
 		else:
 			letter_list = list(word)
-			i = 0
+			counter = 0
 			poped = []
 
 			for letter in letter_list:
@@ -30,9 +28,9 @@ def compair(dic_list,input_word,line):
 					poped_al = input_word.pop(input_word.index(letter))
 					poped.append(poped_al)
 
-					if i+1 == len(word):
+					if counter+1 == len(word):
 						result_list.append(word)
-					i = i + 1
+					counter = counter + 1
 				else:
 					break
 
@@ -41,17 +39,15 @@ def compair(dic_list,input_word,line):
 
 	return result_list	
 
-
-
-starttime  = time.clock()
+starttime   = time.clock()
 
 script,line,string_25 = argv
-dic_list   = read_dic()
-input_word = list(string_25)
 
+dic_list    = read_dic()
+input_word  = list(string_25)
 result_list = compair(dic_list,input_word,line)
 
-endtime    = time.clock()
+endtime     = time.clock()
 
 print result_list
 print 'Time cost: '+str(endtime-starttime)
